8fcbd4a3c469f4525ab2a30e2007c2d32707e5ad
IntDipto <dipto.dey@intelli.global>
2022-11-17 18:40:01 +0600

    author bug fix

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#nav-button').on('click',function(){
+        $('#show-nav').toggleClass('hidden')
+        $('#hide-nav').toggleClass('hidden')
+        $('#nav-items').toggleClass('!opacity-100 !visible');
+        $('.left-half').toggleClass('!top-0 !opacity-100 !visible');
+        $('.right-half').toggleClass('!top-0 !opacity-100 !visible');
+
+    })
+})
